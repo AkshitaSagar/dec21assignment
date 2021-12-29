d970adf0264aae04276ca36172a90640154937bf
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-29 12:01:57 +0530

    Add files via upload

---
+++ 21dec21.js
@@ -0,0 +1,78 @@
+//console.log('hi');
+
+//Take 3 inputs from the user and show minimum of the three.
+
+
+/*var n1=parseInt(prompt("enter 1st number"));
+var n2=parseInt(prompt("enter 2nd number"));
+var n3=parseInt(prompt("enter 3rd number"));
+if(n1<=n2,n1<=n3)
+{
+console.log(n1);
+}else if
+(n2<=n1,n2<=n3)
+console.log(n2);
+else
+{
+console.log(n3);
+}*/
+
+//Check whether the given chracter is a vowel or not. (Also check for capital characters).
+
+var n=prompt('enter alphabet');
+if(n=="a")
+{
+console.log("is a vowel");
+alert("vowel");
+}
+else if(n=="e")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="i")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="o")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="u")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="A")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="E")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="I")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="O")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+else if(n=="U")
+{
+alert("vowel");
+console.log("is a vowel");
+}
+
+else
+ {
+alert("consonant");
+console.log("is a consonant")
+}
